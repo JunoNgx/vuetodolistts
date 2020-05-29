07fbf0e77543ce1e075dd6d5ff416f1ee11af4fc
JunoNgx <juno.ngx@gmail.com>
2020-05-29 20:26:54 +0800

    deploy

---
+++ precache-manifest.42b4cb9ab4a3d70bf41b88a4ad31ebe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069f777d32cf7b3d3432",
+    "url": "/vuetodolistts/css/app.14323e26.css"
+  },
+  {
+    "revision": "52a1084ecb03e197c89d4ffff6a439a9",
+    "url": "/vuetodolistts/index.html"
+  },
+  {
+    "revision": "069f777d32cf7b3d3432",
+    "url": "/vuetodolistts/js/app.92f837a2.js"
+  },
+  {
+    "revision": "d43023700e30f164317a",
+    "url": "/vuetodolistts/js/chunk-vendors.48c9dfed.js"
+  },
+  {
+    "revision": "d6f72d81f96118dec063fb43c808774d",
+    "url": "/vuetodolistts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetodolistts/robots.txt"
+  }
+]);
